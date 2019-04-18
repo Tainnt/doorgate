4162050f666906874df23c39f08cf5bd52e3fe51
tainnt <tainguyen2912@gmail.com>
2019-04-19 00:10:20 +0700

    upgrade login form

---
+++ public/js/login.js
@@ -0,0 +1,65 @@
+function validate(input) {
+    if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            return false;
+        }
+    } else {
+        if ($(input).val().trim() == '') {
+            return false;
+        }
+    }
+}
+
+function showValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).addClass('alert-validate');
+}
+
+function hideValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).removeClass('alert-validate');
+}
+
+$('.validate-form .input100').each(function () {
+    $(this).focus(function () {
+        hideValidate(this);
+    });
+    $(this).keyup(function () {
+        if (event.keyCode === 13) {
+            $('.login100-form-btn').click();
+        }
+    });
+});
+
+$('.login100-form-btn').on('click', function () {
+    var input = $('.validate-input .input100');
+    for (var i = 0; i < input.length; i++) {
+        if (validate(input[i]) == false) {
+            showValidate(input[i]);
+            return false;
+        }
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/login',
+        data: {
+            username: $(input[0]).val(),
+            password: $(input[1]).val()
+        },
+        dataType: 'json',
+        async: false,
+        success: function (response) {
+            if (response.status == 'granted') {
+                alert('Đăng nhập thành công');
+                window.location = '/management';
+            } else {
+                alert('Đăng nhập thất bại');
+                $(input[0]).val("");
+                $(input[1]).val("");
+            }
+        }
+    });
+});
--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-///////////////////////////////////////////////////////
-/////////////PHẦN HOAT DONG SERVER/////////////////////
-//////////////////////////////////////////////////////
 var express = require("express");
 var session = require("express-session")
 var cookieParser = require('cookie-parser');
@@ -13,19 +10,41 @@ let sess;
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.use(cookieParser());
 app.use(session({
-    secret: 'hahahihi8585',
+    secret: 'gatedoor',
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 }
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
 }))
 
-server.listen(8080, function() {
+server.listen(8080, function () {
     console.log('Server is running at port: 8080!')
 });
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/login.html');
 });
 
+app.get('/management', function (req, res) {
+    res.sendFile(__dirname + '/views/management.html');
+});
+
+app.post('/login', function (req, res) {
+
+    console.log(req.body);
+    if (req.body.username == 'a' && req.body.password == 'a') {
+        res.send({
+            status: 'granted'
+        });
+    } else {
+        res.send({
+            status: 'denied'
+        });
+    }
+});
